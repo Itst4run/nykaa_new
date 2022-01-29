3d7c88d20e9f879886599091252d2e87632ad1b5
Itst4run <tarkr7@gmail.com>
2022-01-29 16:47:47 +0530

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,21 @@ import './App.css';
 import Content from './components/Content';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import {Routes,Route,BrowserRouter} from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Signin from './components/Signin';
 function App() {
   return (
-    
-    <BrowserRouter> 
-    <div className="App">
-     <Header/>
-      <Routes>
-         <Route path="/" element={<Content/>}/>
-        <Route path="/signin" element={<Signin/>}/>
-       
-  
-        
-      </Routes>
-     <Footer/>
-    </div>
-      </BrowserRouter>
+
+    <BrowserRouter>
+      <div className="App">
+
+        <Routes>
+          <Route path="/" element={<Content />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
 
   );
 }
--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from "../styles/Content.module.css"
+import Layout from './Layout';
 import Product1 from './Product1';
 import Slider from './Slider';
 
 export default function Content() {
   return (
-    <div><Slider/>
-  <div className={styles.content}>
- 
+    <Layout>
+      <div><Slider />
+        <div className={styles.content}>
 
-    <Product1/>
-  </div>
-  </div>
-    )
+
+          <Product1 />
+        </div>
+      </div>
+    </Layout>
+  )
 }
