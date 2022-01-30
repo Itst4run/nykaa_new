3947e31bc5e9ba8278b06fa70cfcbd05438ac05b
Itst4run <tarkr7@gmail.com>
2022-01-30 16:53:06 +0530

    a

--- frontend/src/components/product.js
+++ frontend/src/components/product.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faHeart } from '@fortawesome/free-solid-svg-icons'
 export default function Product({name,image,price,rating}) {
   return (<div className="product">
     <div className="header">
@@ -10,10 +12,10 @@ export default function Product({name,image,price,rating}) {
     </div>
     <div className="name">{name}</div>
     {/* <div className="category">category</div> */}
-    <div className="price">MRP:{price}</div>
+    <div className="price">MRP:₹  {price}</div>
     <div className="rating">rating:{rating}</div>
     <div className="btn">
-      <div className="icon">icon</div>
+      <div className="icon"><FontAwesomeIcon icon={faHeart}  /></div>
       <button className='add-to'>ADD TO BAG</button>
     </div>
   </div>)
